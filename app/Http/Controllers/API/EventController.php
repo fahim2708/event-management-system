cfcea4f066be246123980690b23e6afedf2aea2a
thedevfahim <fahimulislam.official@gmail.com>
2024-07-04 22:22:26 +0600

    developed api endpoints

---
+++ app/Http/Controllers/API/EventController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Interfaces\EventRepositoryInterface;
+use App\Classes\ApiResponse;
+use App\Http\Resources\EventResource;
+use Illuminate\Support\Facades\DB;
+
+class EventController extends Controller
+{
+
+    private EventRepositoryInterface $eventRepositoryInterface;
+    
+    public function __construct(EventRepositoryInterface $eventRepositoryInterface)
+    {
+        $this->eventRepositoryInterface = $eventRepositoryInterface;
+    }
+    
+    //Retrieve a list of all events
+    public function index()
+    {
+        $data = $this->eventRepositoryInterface->index();
+
+        return ApiResponse::sendResponse(EventResource::collection($data),'',200);
+    }
+
+    //Create a new event
+    public function store(StoreEventRequest $request)
+    {
+        $data =[
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'location' => $request->location
+        ];
+        DB::beginTransaction();
+        try{
+             $event = $this->eventRepositoryInterface->store($data);
+
+             DB::commit();
+             return ApiResponse::sendResponse(new EventResource($event),'Event Created Successfully',201);
+
+        }catch(\Exception $e){
+            return ApiResponse::rollback($e);
+        }
+    }
+
+    //Retrieve details of a specific event
+    public function show($id)
+    {
+        $product = $this->eventRepositoryInterface->getById($id);
+
+        return ApiResponse::sendResponse(new EventResource($product),'',200);
+    }
+
+
+    //Update an existing event
+    public function update(UpdateEventRequest $request, $id)
+    {
+        $updateData =[
+            'title' => $request->title,
+            'description' => $request->description,
+            'date' => $request->date,
+            'location' => $request->location
+        ];
+        DB::beginTransaction();
+        try{
+             $events = $this->eventRepositoryInterface->update($updateData,$id);
+
+             DB::commit();
+             return ApiResponse::sendResponse('Event Updated Successfully','',201);
+
+        }catch(\Exception $e){
+            return ApiResponse::rollback($e);
+        }
+    }
+
+    //Delete an event
+    public function destroy($id)
+    {
+         $this->eventRepositoryInterface->delete($id);
+
+        return ApiResponse::sendResponse('Event Deleted Successfully','',200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\API\Auth\LoginController;
 use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\EventController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 //User Authentication
 Route::post('register', RegisterController::class);
 Route::post('login', LoginController::class);
+
+
+Route::middleware('auth:sanctum')->group ( function()   {
+    Route::prefix('events')->group(function () {
+        Route::get('/view', [EventController::class, 'index']);
+        Route::post('/create', [EventController::class, 'store']);
+        Route::get('/show/{id}', [EventController::class, 'show']);
+        Route::put('/edit/{id}', [EventController::class, 'update']);
+        Route::delete('/delete/{id}', [EventController::class, 'destroy']);
+    });
+
+});
